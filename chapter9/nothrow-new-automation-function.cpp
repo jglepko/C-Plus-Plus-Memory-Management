91099f97bcfdec127fa1dabf9cd318ca89182a40
Joshua Glepko <jglepko@gmail.com>
2025-06-13 10:20:33 -0400

    added ch9 files

---
+++ chapter9/nothrow-new-automation-function.cpp
@@ -0,0 +1,20 @@
+#include <new>
+#include <cstdlib>
+#include <utility>
+
+template <class T, class ... Args>
+    auto try_new(Args &&... args) {
+        auto p = new (std::nothrow) T(std::forward<Args>(args)...);
+        if(!p) std::abort;
+        return p;
+    }
+
+struct dies_when_newed {
+    void* operator new(std::size_t, std::nothrow_t) { return {}; }
+};
+
+int main() {
+    auto p0 = try_new<int>(); // p0 is int*, points an int{ 0 }
+    auto p1 = try_new<int>(3); // p1 is int*, points an int{ 3 }
+    auto q = try_new<dies_when_newed>(); // calls abort()
+}
